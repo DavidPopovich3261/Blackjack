05b9d51530430f94c6d946a549abd9cb862dfe6e
DavidPopovich3261 <8534397@gmail.com>
2025-10-30 11:23:40 +0200

    Finished project

--- core/player_io.py
+++ core/player_io.py
@@ -4,7 +4,6 @@
 def ask_player_action() -> str:
     while True:
         action=input('Please select S or H.')
-        action=action.upper()
         if  action in ('S','H'):
             return action
 
