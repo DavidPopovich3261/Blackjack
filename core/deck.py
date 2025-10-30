5066b35fc3308863f1feeb3f687f39859e19d427
DavidPopovich3261 <8534397@gmail.com>
2025-10-30 09:34:27 +0200

    build_standard_deck

--- core/deck.py
+++ core/deck.py
@@ -2,9 +2,14 @@
 
 
 def build_standard_deck() -> list[dict]:
-    pass
-
-
+    rank=["a","2",'3','4','5','6','7','8','9','10','j','q','k']
+    suite=["h",'c','d','s']
+    deck = []
+    for i in rank:
+        for j in suite:
+            card={'rank':i,'suite':j}
+            deck.append(card)
+    return deck
 
 def shuffle_by_suit(deck: list[dict], swaps: int = 5000) -> list[dict]:
     pass
--- core/game_logic.py
+++ core/game_logic.py
@@ -3,6 +3,7 @@
 
 
 def calculate_hand_value(hand: list[dict]) -> int:
+    # rank={'a':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,"8":8,'9':9,'10':10,'j':10,"q":10,'k':10}
     pass
 
 
